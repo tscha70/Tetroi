813b6d927aed00a4921d26d29deffae27a98bb18
Roger Rabbit <tscha70@gmail.com>
2023-12-19 16:44:03 +0100

    Update Tetroi.ino

--- Tetroi/Tetroi.ino
+++ Tetroi/Tetroi.ino
@@ -1,3 +1,20 @@
+//   >>>>> Tetroi version 1 for ARDUBOY  GPLv3 <<<<
+//         Programmer: (c) Roger Buehler 2023
+//         Contact EMAIL: tscha70@gmail.com
+//         Official repository:  https://github.com/tscha70/
+//  Tetroi version 1 is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+
+//  You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #include <Arduboy2.h>
 #include <ArduboyTones.h>
 #include <EEPROM.h>
